8369d06eec33977ad60f50691c3e0fc45e840446
leehyelim0691 <qazxcv0691@naver.com>
2021-10-08 23:01:48 +0900

    create form function

--- form/src/App.js
+++ form/src/App.js
@@ -1,14 +1,10 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import Main from './Page/Main.js';
-import Practice from './Page/Practice.js';
-import Practice1 from './Page/Practice1.js';
 
 function App() {
   return (
-    // <Practice/>
     <Main/>
-    // <Main/>
   );
 }
 
--- form/src/Page/Main.js
+++ form/src/Page/Main.js
@@ -1,75 +1,50 @@
-import { Container, Row, Col, InputGroup, Button, Input, Form, FormGroup, Label, FormText, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, ButtonGroup  } from 'reactstrap';
-import React, { useState, useRef, useCallback, setValue } from 'react';
+import { Container, Row, Col, Button, Form, FormGroup, Label} from 'reactstrap';
+import React, { useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 
 function Main() {
   const [rSelected, setRSelected] = useState(null);
-  const [age, setAge] = useState(0);
+  const [form, setForm] = useState('');
   const json = [
-    '{\n\t"field_id": "",\n\t"field_label": "",\n\t"field_mandatory": "",\n\t"field_placeholder": "",\n\t"field_value": "",\n\t"field_type": "text"\n},',
-    '{\n\t"field_id": "",\n\t"field_label": "",\n\t"field_value": "",\n\t"field_mandatory": "",\n\t"field_options": [\n\t\t{\n\t\t\t"option_label": ""\n\t\t}\n\t],\n\t"field_type": "select"\n},',
-    '{\n\t"field_id": "",\n\t"field_label": "",\n\t"field_value": ""\n\t"field_type": "checkbox"\n},'
+    '{\n\t"field_id": "",\n\t"field_label": "",\n\t"field_mandatory": "",\n\t"field_placeholder": "",\n\t"field_value": "",\n\t"field_type": "text"\n},\n',
+    '{\n\t"field_id": "",\n\t"field_label": "",\n\t"field_value": "",\n\t"field_mandatory": "",\n\t"field_options": [\n\t\t{\n\t\t\t"option_label": ""\n\t\t}\n\t],\n\t"field_type": "select"\n},\n',
+    '{\n\t"field_id": "",\n\t"field_label": "",\n\t"field_value": ""\n\t"field_type": "checkbox"\n},\n',
+    '{\n\t"field_id": "",\n\t"field_label": "",\n\t"field_value": ""\n\t"field_type": "color"\n},\n',
+    '{\n\t"field_id": "",\n\t"field_label": "",\n\t"field_value": ""\n\t"field_type": "search"\n},\n',
+    '{\n\t"field_id": "",\n\t"field_label": "",\n\t"field_value": ""\n\t"field_type": "email"\n},\n'
   ]
-
-  const updateAge = (value) => {
-    setAge({age: value}, checkAge);
-  };
-
-  const checkAge = () => {
-    // const { age } = setAge;
-    if (age !== 0 && age >= 21) {
-      // Make API call to /beer
-    } else {
-      // Throw error 404, beer not found
-    }
-  };
+  const handleChange = (e) => {
+      setForm(e.target.value);
+  }
+  const onClick = (e) => {
+    setRSelected(e); 
+    setForm(form+json[e]);
+  }
 
   return (
     <Container>
         <FormGroup>
             <Label className="inputType">Input Type</Label>
             <Form>
-              <Button outline color="danger" onClick={() => setRSelected(0)} active={rSelected === 0}>Text</Button>{' '}
-              <Button outline color="warning" onClick={() => setRSelected(1)} active={rSelected === 1}>Select</Button>{' '}
-              <Button outline color="success" onClick={() => setRSelected(2)} active={rSelected === 2}>Checkbox</Button>{' '}
-              <Button outline color="info" onClick={() => setRSelected(3)} active={rSelected === 3}>Color</Button>{' '}
-              <Button outline color="primary" onClick={() => setRSelected(4)} active={rSelected === 4}>Search</Button>{' '}
-              <Button outline color="secondary" onClick={() => setRSelected(5)} active={rSelected === 5}>Email</Button>{' '}
+              <Button outline color="danger" onClick={() => onClick(0)} active={rSelected === 0}>Text</Button>{' '}
+              <Button outline color="warning" onClick={() =>onClick(1)} active={rSelected === 1}>Select</Button>{' '}
+              <Button outline color="success" onClick={() =>onClick(2)} active={rSelected === 2}>Checkbox</Button>{' '}
+              <Button outline color="info" onClick={() =>onClick(3)} active={rSelected === 3}>Color</Button>{' '}
+              <Button outline color="primary" onClick={() =>onClick(4)} active={rSelected === 4}>Search</Button>{' '}
+              <Button outline color="secondary" onClick={() =>onClick(5)} active={rSelected === 5}>Email</Button>{' '}
               <hr/>
             </Form>
-            {/* <p>Selected: {rSelected}</p> */}
             <Row>
               <Col>
                 <TextareaAutosize cols="100" 
-                  placeholder={rSelected} 
-                  // value={rSelected} 
-                  value={json[rSelected]}
-                  onChange={e => updateAge(e.target.value)}
+                  value={form}
+                  onChange={handleChange}
                 />
               </Col>
               <Col>
                 <Button outline color="danger">Submit</Button>{' '}
               </Col>
             </Row>
-            
-            {/* <div>
-                {cSelected.map(type =>{     
-                    return (
-                      <FormGroup className="form">
-                          <Label for={type}>{type}</Label>
-                          <br/>
-                          <Input
-                              type={type}
-                              name={type}
-                              id={type}
-                              placeholder={type}
-                              value = {value}
-                              onChange = {onChange11}
-                          />
-                      </FormGroup>
-                    );
-                })}
-            </div> */}
         </FormGroup>
     </Container>
   );      
