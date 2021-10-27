de97c2de8abf9466910eb267f6b9f68849f569bb
leehyelim0691 <qazxcv0691@naver.com>
2021-10-27 11:57:19 +0900

    revise create

--- IF_Project/json-form/src/page/Main.js
+++ IF_Project/json-form/src/page/Main.js
@@ -15,9 +15,6 @@ function Main() {
   const [rSelected, setRSelected] = useState(null);
   const [json, setJson] = useState(null);
   const [count, setCount] = useState(0);
-  const resetInput = useRef();
-
-  
   const { fields, page_label } = elements ?? {}
 
   useEffect(() => {
@@ -29,10 +26,7 @@ function Main() {
   }
 
   const onClickCreate = () => {
-    // var schema = document.getElementById('jsonEditor').value+"\n]}";
     var schema = document.getElementById('json-editor').value;
-
-    // setForm(form + "\n]}");
     console.log(schema);
     var myobj=JSON.parse(schema);
     setElements(myobj);
@@ -42,7 +36,7 @@ function Main() {
 
   const onClickReset = () => {
     setForm('{\n"page_label": "이력서 Form",\n"fields": [\n ]}');
-    resetInput.current.focus();
+    setCount(0);
   };
 
   const handleChange = (id, event) => {
@@ -69,38 +63,40 @@ function Main() {
     setRSelected(e); 
 
     if(count == 0){
-      // setForm(form); 
       var txtArea =  document.getElementById('json-editor');
       var txtValue = txtArea.value;
-      var selectPos = txtValue.length-2; // 커서 위치 지정
-      // selectPos = 
-      var beforeTxt = txtValue.substring(0, selectPos);  // 기존텍스트 ~ 커서시작점 까지의 문자
-      var afterTxt = txtValue.substring(txtArea.selectionEnd+txtValue.length-2, txtValue.length);   // 커서끝지점 ~ 기존텍스트 까지의 문자
-      var addTxt = JSON.stringify(json[e],null, 4) + '\n'; // 추가 입력 할 텍스트
+      var selectPos = txtValue.length-2;
+      var beforeTxt = txtValue.substring(0, selectPos);
+      var afterTxt = txtValue.substring(txtArea.selectionEnd+txtValue.length-2, txtValue.length); 
+      var addTxt = JSON.stringify(json[e],null, 4) + '\n'; 
 
       setForm(beforeTxt + addTxt + afterTxt);
       txtArea.value = beforeTxt + addTxt + afterTxt;
-
       selectPos = selectPos + addTxt.length;
-      // txtArea.selectionStart = selectPos; // 커서 시작점을 추가 삽입된 텍스트 이후로 지정
-      txtArea.selectionEnd = selectPos; // 커서 끝지점을 추가 삽입된 텍스트 이후로 지정
-      // 'json-editor'.focus();
+      txtArea.selectionEnd = selectPos;
     }
     else{
       var txtArea =  document.getElementById('json-editor');
       var txtValue = txtArea.value;
-      var selectPos = txtArea.selectionStart; // 커서 위치 지정
-      var beforeTxt = txtValue.substring(0, selectPos);  // 기존텍스트 ~ 커서시작점 까지의 문자
-      var afterTxt = txtValue.substring(txtArea.selectionEnd, txtValue.length);   // 커서끝지점 ~ 기존텍스트 까지의 문자
-      var addTxt = ',' + JSON.stringify(json[e],null, 4)+ '\n'; // 추가 입력 할 텍스트
-
-      setForm(beforeTxt + addTxt + afterTxt);
-      txtArea.value = beforeTxt + addTxt + afterTxt;
-
-      selectPos = selectPos + addTxt.length;
-      // txtArea.selectionStart = selectPos; // 커서 시작점을 추가 삽입된 텍스트 이후로 지정
-      txtArea.selectionEnd = selectPos; // 커서 끝지점을 추가 삽입된 텍스트 이후로 지정
-      // 'json-editor'.focus();
+      var selectPos = txtArea.selectionStart; 
+      if(txtValue.substring(selectPos-1,selectPos)=='[' || txtValue.substring(selectPos-2,selectPos-1)=='['){
+        var beforeTxt = txtValue.substring(0, selectPos); 
+        var afterTxt = txtValue.substring(txtArea.selectionEnd, txtValue.length);  
+        var addTxt = '\n' + JSON.stringify(json[e],null, 4) + ',';
+        setForm(beforeTxt + addTxt + afterTxt);
+        txtArea.value = beforeTxt + addTxt + afterTxt;
+        selectPos = selectPos + addTxt.length;
+        txtArea.selectionEnd = selectPos; 
+      }
+      else{
+        var beforeTxt = txtValue.substring(0, selectPos);  
+        var afterTxt = txtValue.substring(txtArea.selectionEnd, txtValue.length);  
+        var addTxt = ',' + JSON.stringify(json[e],null, 4)+ '\n'; 
+        setForm(beforeTxt + addTxt + afterTxt);
+        txtArea.value = beforeTxt + addTxt + afterTxt;
+        selectPos = selectPos + addTxt.length;
+        txtArea.selectionEnd = selectPos; 
+      }
     }
     setCount(count+1);
   }
@@ -134,7 +130,6 @@ function Main() {
       <div className="editor-container">
         <div className="editor-box" >
           <div className="editor-head"><h5>JSONSchema</h5></div> 
-            {/* <textarea className="json-editor" id="json-editor" value={form} onChange={textChange}>{schema}</textarea> */}
             <textarea className="json-editor" id="json-editor" value={form} onChange={textChange}>{schema}</textarea>
             <Row>
               <Col>
@@ -152,9 +147,6 @@ function Main() {
               {fields ? fields.map((field, i) => <Element key={i} field={field} />) : null}
             </form>
           :null}  
-           <input
-        type="text"
-        ref={resetInput} />
         </div> 
       </div>
     </div>  
