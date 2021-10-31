34a224e82bd1b807dcba3ca9ac439aaa821300a1
susannajung <21800662@handong.edu>
2021-10-31 22:36:09 +0900

    merge

--- IF_Project/json-form/src/components/elements/Color.js
+++ IF_Project/json-form/src/components/elements/Color.js
@@ -4,9 +4,9 @@ import { FormContext } from '../../FormContext';
 const Color = ({ id, label, value, width }) => {
     const { handleChange } = useContext(FormContext)
     return (
-        <div class={width? 'mb-3 col-md-'+width: 'mb-3 form-control-color'}>
+        <div >
             <label htmlFor="favcolor" className="form-label">{label}</label>
-            <input type="color" className="form-control" id="favcolor" 
+            <input type="color" class={width? 'mb-3 col-md-'+width: 'mb-3 form-control-color'} className="form-control" id="favcolor" 
                 value={value}
                 onChange={event => handleChange(id, event)}
             />
