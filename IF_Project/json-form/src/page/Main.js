72bd4f9a8ac3c1c421bfaadeaed39e62d93a9f32
leehyelim0691 <qazxcv0691@naver.com>
2021-11-02 22:11:26 +0900

    add group

--- IF_Project/json-form/src/page/Main.js
+++ IF_Project/json-form/src/page/Main.js
@@ -7,72 +7,30 @@ import Element from '../components/Element';
 import { FormContext } from '../FormContext';
 import jsonSkeleton from '../components/elements/jsonSkeleton.json';
 import axios from 'axios';
-import JSONInput from 'react-json-editor-ajrm';
-import locale    from 'react-json-editor-ajrm/locale/en';
-import { objectExpression } from '@babel/types';
+
+
+/*
+{
+"page_label": "이력서 Form",
+"group":
+  [
+    "group_name" : ""
+    "fields":[
+    ]
+  ]
+}
+ */
 
 function Main() {
   const [clicked, setClicked] = useState(false);
   const [schema,setSchema] = useState();
   const [elements, setElements] = useState(null);
   const ID = 0;
-  //const [form, setForm] = useState('{\n"page_label": "이력서 Form",\n"fields": [\n ]}');
-  const [form, setForm] = useState({"page_label": "Form 1","fields": []});
-  //const [form, setForm] = useState('{"page_label": "이력서 Form","group": [{"group_name": "","fields": []}]}');
+  const [form, setForm] = useState('{\n\t"page_label" : "이력서 Form",\n\t"group" : [\n\t\t{\n\t\t\t"group_name" : "",\n\t\t\t"fields" : [\n\t\t\t ]\n\t\t}\n\t]\n}');
   const [rSelected, setRSelected] = useState(null);
   const [json, setJson] = useState(null);
   const [count, setCount] = useState(0);
   const { fields, page_label } = elements ?? {}
-  // const form3 = form.slice(1,form.length-1);
-  // const [form1, setForm1] = useState('{"page_label": "이력서 Form","fields": [{"id": "date_1",},{"id": "datetime-local_1",}]}');
-  // const form1={
-    // "page_label": "이력서 Form",
-    // "fields": [
-    //  {
-    //     "id": "date_1",
-    //     "label": "",
-    //     "mandatory": "",
-    //     "value": "",
-    //     "min": "",
-    //     "filed_max": "",
-    //     "type": "date",
-    //     "width": ""
-    // }
-    // ,{
-    //     "id": "datetime-local_1",
-    //     "label": "",
-    //     "mandatory": "",
-    //     "value": "",
-    //     "min": "",
-    //     "filed_max": "",
-    //     "type": "datetime-local",
-    //     "width": ""
-    // }
-    // ]}
-  const form2={
-    "fields": [
-      {
-         "id": "date_1",
-         "label": "",
-         "mandatory": "",
-         "value": "",
-         "min": "",
-         "filed_max": "",
-         "type": "date",
-         "width": ""
-     }
-     ,{
-         "id": "datetime-local_1",
-         "label": "",
-         "mandatory": "",
-         "value": "",
-         "min": "",
-         "filed_max": "",
-         "type": "datetime-local",
-         "width": ""
-     }
-     ]
-  }
   const [error, setError] = useState(null);
   const [nums, setNums] = useState([
     {
@@ -135,72 +93,38 @@ function Main() {
       id: 14,
       id_num: 1
     },
-    
+    {
+      id: 15,
+      id_num: 1
+    },
   ]);
 
   useEffect(() => {
     setJson(jsonSkeleton);
   },[]);
   
-  const textChange = (JsonData) => {
-    setForm(JsonData);
+  const textChange = (e) => {
+    setForm(e.target.value);
   }
 
   const onClickCreate = () => {
-    // var schema = document.getElementById('json-editor');
-    // console.log(schema);
-    // var myobj=JSON.parse(schema);
-    console.log(form);
-    setElements(form);
+    var schema = document.getElementById('json-editor').value;
+    console.log(schema);
+    var myobj=JSON.parse(schema);
+    setElements(myobj);
     setClicked(true);
-    // setError ("동일한 아이디가 존재합니다.");
-
-    // form.map((input_type) =>(
-    //   form.map((input_type1) =>{
-    //     if(input_type.id==input_type1.id1){
-    //       setError ("동일한 아이디가 존재합니다.");
-    //     }
-    //   })
-    // ))
-
   };
-  const mydata = {
-    "myDeviceData":[
-      {
-        "name":"iPad Pro",
-        "RAM":6,
-        "HomeButton":false,
-        "TouchID":"No",
-        "FaceID":"Yes"
-      },{
-        "name":"iPhone Xs",
-        "RAM":4,
-        "HomeButton":false,
-        "TouchID":"No",
-        "FaceID":"Yes"
-      },{
-        "name":"iPhone 6",
-        "RAM":1,
-        "HomeButton":true,
-        "TouchID":"Yes",
-        "FaceID":"No"
-      }
-    ]
-};
-
-
+  
   const onClickReset = () => {
-    var reset = {"page_label":'Form 1',fields:[]};
-    setForm(reset);
-    //setForm('{\n"page_label": "이력서 Form",\n"group": [\n{\n"group_name": "",\n"fields": [\n]}\n]}');
+    setForm('{\n"page_label": "이력서 Form",\n"fields": [\n ]}');
     setCount(0);
   };
 
   const onClickSave = () => {
-    // console.log(form);
-    // var data=JSON.parse(form);
-    // console.log(data);
-    axios.post('http://localhost:3002/api/upload',form)
+    console.log(form);
+    var data=JSON.parse(form);
+    console.log(data);
+    axios.post('http://localhost:3002/api/upload',data)
     //성공시 then 실행
     .then(function (response) {
       console.log(response);
@@ -210,25 +134,25 @@ function Main() {
     .catch(function (error) {
       console.log(error);
     });
+
   };
 
   const onClickDownload = () => {
+    var formObj=JSON.parse(form);
     const element = document.createElement("a");
     const file = new Blob([document.getElementById('json-editor').value], {type: 'text/plain'});
     element.href = URL.createObjectURL(file);
-    element.download = form.page_label+".txt";
+    element.download = formObj.page_label+".txt";
     document.body.appendChild(element); // Required for this to work in FireFox
     element.click();
   };
 
+  const onHi = () => {
+    // alert('hihi');
+  }
+
+
   const handleChange = (ID, event) => {
-    // form.map((input_type) =>(
-    //   form.map((input_type1) =>{
-    //     if(input_type.id==input_type1.id1){
-    //       setError ("동일한 아이디가 존재합니다.");
-    //     }
-    //   })
-    // ))
     const newElements = { ...elements }
     newElements.fields.forEach(field => {
       const { type, id } = field;
@@ -249,80 +173,101 @@ function Main() {
   }
 
   const addJson = e => {
-    setNums(
-      nums.map(num =>
-        num.id === e ? { ...num, id_num : num.id_num+1} : num
-      )
-    );
-
-    nums.map(num =>
-      num.id == e ? (json[e].id = json[e].type + '_' + num.id_num) : (json[e].id = json[e].id)
-    );
-
     setRSelected(e);
-    
-    if(count == 0){
-      var temp = {...form,fields:[...form.fields]};
-      temp.fields.push(json[e]);
-      console.log(temp);
-      textChange(temp);
-    }
-    
-/*
-    if(count == 0){
+
+    if(e == 15){
       var txtArea =  document.getElementById('json-editor');
       var txtValue = txtArea.value;
-      var selectPos = txtValue.length-2;
+      txtArea.selectionFront = selectPos;
+      var selectPos = txtValue.length-5;
       var beforeTxt = txtValue.substring(0, selectPos);
-      var afterTxt = txtValue.substring(txtArea.selectionEnd+txtValue.length-2, txtValue.length);     
-      if(afterTxt=='') afterTxt = ']}';  
-      var addTxt = JSON.stringify(json[e],null, 4) + '\n';
+      var addTxt = ',\n\t\t{\n\t\t\t"group_name" : "",\n\t\t\t"fields" : [\n\t\t\t]\n\t\t}';
+      var afterTxt =   txtValue.substring(selectPos, txtValue.length);
       setForm(beforeTxt + addTxt + afterTxt);
-      txtArea.value = beforeTxt + addTxt + afterTxt;
-      selectPos = selectPos + addTxt.length;
-      txtArea.selectionEnd = selectPos;
     }
     else{
-      var txtArea =  document.getElementById('json-editor');
-      var txtValue = txtArea.value;
-      var selectPos = txtArea.selectionStart; 
-      var check;
-      if(selectPos=='') {
-        selectPos = txtValue.length-2;
-        check = 1;
-      }
-      if(txtValue.substring(selectPos-1,selectPos)=='[' || txtValue.substring(selectPos-2,selectPos-1)=='['){
-        var beforeTxt = txtValue.substring(0, selectPos); 
-        var afterTxt = txtValue.substring(txtArea.selectionEnd, txtValue.length);  
-        var addTxt = '\n' + JSON.stringify(json[e],null, 4) + ',';
-        setForm(beforeTxt + addTxt + afterTxt);
+      setNums( 
+        nums.map(num =>
+          num.id === e ? { ...num, id_num : num.id_num+1} : num
+        )
+      );
+
+      nums.map(num =>
+        num.id === e ? (json[e].id = json[e].type + '_' + num.id_num) : (json[e].id = json[e].id)
+      );
+
+      if(count == 0){
+        var txtArea =  document.getElementById('json-editor');
+        var txtValue = txtArea.value;
+        // var selectPos = txtValue.length-2;
+        var selectPos = txtValue.length-10;
+        var beforeTxt = txtValue.substring(0, selectPos);
+        var afterTxt = txtValue.substring(txtArea.selectionEnd+txtValue.length-14, txtValue.length);     
+        if(afterTxt=='') afterTxt = ']}';  
+        // var jsonLength = Object.keys(json[e]).length;
+        // var jsonTxt = JSON.stringify(json[e],null, 4);
+        // var jsonTxt = JSON.stringify(json[e]);
+        // setForm(jsonTxt);
+
+      //   for (var i = 0; i < jsonLength; i++) {
+      //     addTxt += "\t" + jsonTxt[i];
+      // }
+
+      // var jsonKeys = Object.keys(ObjData);
+      // var addTxt;
+      // for(key in jsonKeys{
+      //   addTxt = key + " : " + json[e][key].first_name+" , last Name :"+data[key].last_name";
+      // }
+
+        var addTxt = JSON.stringify(json[e],null, 4) + '\n'; 
+        // setForm(beforeTxt + addTxt + '\n after is \n'+afterTxt);
+        setForm(beforeTxt + addTxt +afterTxt);
         txtArea.value = beforeTxt + addTxt + afterTxt;
-        selectPos = selectPos + addTxt.length;
-        txtArea.selectionEnd = selectPos; 
+        // selectPos = selectPos + addTxt.length;
+        txtArea.selectionEnd = selectPos;
       }
       else{
-        var beforeTxt = txtValue.substring(0, selectPos); 
-        if(check==1) var afterTxt=']}';
-        else var afterTxt = txtValue.substring(txtArea.selectionEnd, txtValue.length); 
-        var addTxt = ',' + JSON.stringify(json[e],null, 4)+ '\n'; 
-        setForm(beforeTxt+addTxt+afterTxt);
-        txtArea.value = beforeTxt + addTxt + afterTxt;
-        selectPos = selectPos + addTxt.length;
-        txtArea.selectionEnd = selectPos; 
+        var txtArea =  document.getElementById('json-editor');
+        var txtValue = txtArea.value;
+        var selectPos = txtArea.selectionStart; 
+        var check;
+        if(selectPos=='') {
+          selectPos = txtValue.length-2;
+          check = 1;
+        }
+        if(txtValue.substring(selectPos-1,selectPos)=='[' || txtValue.substring(selectPos-2,selectPos-1)=='['){
+          var beforeTxt = txtValue.substring(0, selectPos); 
+          var afterTxt = txtValue.substring(txtArea.selectionEnd, txtValue.length);  
+          var addTxt = '\n' + JSON.stringify(json[e],null, 4) + ','+'444'; 
+          setForm(beforeTxt + addTxt + afterTxt);
+          txtArea.value = beforeTxt + addTxt + afterTxt;
+          selectPos = selectPos + addTxt.length;
+          txtArea.selectionEnd = selectPos; 
+        }
+        else{
+          var beforeTxt = txtValue.substring(0, selectPos); 
+          if(check==1) var afterTxt=']}';
+          else var afterTxt = txtValue.substring(txtArea.selectionEnd, txtValue.length); 
+          var addTxt = ',' + JSON.stringify(json[e],null, 4)+ '\n'+'555'; 
+          setForm(beforeTxt+addTxt+afterTxt);
+          txtArea.value = beforeTxt + addTxt + afterTxt;
+          selectPos = selectPos + addTxt.length;
+          txtArea.selectionEnd = selectPos; 
+        }
       }
+      setCount(count+1);
     }
-    setCount(count+1);
-    */
   }
     
   return (
     <FormContext.Provider value={{ handleChange }}>
+      {/* {onHi()} */}
     <div className="App">
       <Container>
         <FormGroup>
             <Label className="mb-3 inputType">Select the Form Element you want to create</Label>
             <Form className="mb-5">
-              <Button outline color="danger" onClick={() => addJson(0)} active={rSelected === 0}>Group</Button>{' '}
+              <Button outline color="danger" onClick={() => addJson(15)} active={rSelected === 15}>Group</Button>{' '}
               <Button outline color="primary" onClick={() => addJson(0)} active={rSelected === 0}>Text</Button>{' '}
               <Button outline color="primary" onClick={() =>addJson(4)} active={rSelected === 4}>Email</Button>{' '}
               <Button outline color="primary" onClick={() =>addJson(10)} active={rSelected === 10}>Telephone</Button>{' '}
@@ -349,28 +294,7 @@ function Main() {
             <h5>JSONSchema</h5>
             <Button className="button-run" aria-label="Run" onClick={() => {onClickCreate()}}><FaPlay /></Button>
           </div> 
-            {/* <textarea className="json-editor" id="json-editor" value={form} onChange={textChange}>{schema}</textarea> */}
-            <JSONInput 
-              id="json-editor" 
-              onBlur={form} //이게 textarea에서 value 값
-              placeholder = {form}
-              colors={{
-                string: "#DAA520"
-              }}
-              locale      = { locale }
-              height      = '100%'
-              width      = '100%'
-              onChange = {textChange}
-            />
-            
-            {/* <Row>
-              <Col>
-              <button className="btn btn-large btn-secondary create-btn" onClick={() => {onClickReset()}}>Reset</button>              
-              </Col>
-              <Col>
-                <button className="btn btn-large btn-secondary create-btn" onClick={() => {onClickCreate()}}>Create</button>
-              </Col>
-            </Row> */}
+            <textarea className="json-editor" id="json-editor" value={form} onChange={textChange}>{schema}</textarea>
           </div>
           <div className="form-box"> 
             <div className="new-form">
@@ -390,45 +314,6 @@ function Main() {
               <Col>
                 <button className="btn btn-large btn-secondary create-btn" onClick={() => {onClickDownload()}}>Download</button>
               </Col>
-              {/* <Col> */}
-              {/* {error} */}
-              <div>
-                {/* {
-                  form.map((f) => {
-                    return (
-                      <div>
-                        안녕? {f.id}
-                      </div>
-                    )
-                  }
-                  )
-                } */}
-                <div>
-                {/* {alert('Click!')} */}
-            {
-                // form && form.fields.map((myAppleDevice) => {
-                //     console.log('idx is ' + JSON.stringify(myAppleDevice.name));
-                //     return(
-                //         <div>
-                //             이름 : { myAppleDevice.name } <br></br>
-                //             램 : { myAppleDevice.RAM }GB<br></br>
-                //             홈버튼 : { ((myAppleDevice.HomeButton === true) ? '있음' : '없음') } <br></br>
-                //             터치 ID : { myAppleDevice.TouchID } <br></br>
-                //             페이스 ID : { myAppleDevice.FaceID } <br></br><br></br>
-                //          </div>
-                //     );
-                // })
-            }
-            {/* {form2} */}
-            {/* {form} */}
-            {/* form1,3은 {} 없음 */}
-            {/* ;;;;;;;;;;;;;;;;;; */}
-            {/* {form3} */}
-            {/* {form1} */}
-            {/* {form.fields} */}
-        </div>
-              </div>
-              {/* </Col> */}
             </Row>
         </div>
       </div>
